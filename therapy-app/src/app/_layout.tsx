8aa7b02c67fde9e0f7f22f58837cdf42598f80f8
EmiLy-Ly-san <emilie.deduyver@gmail.com>
2026-02-17 11:58:35 +0100

    adapt navigation bar to be visible on mobile

--- therapy-app/src/app/_layout.tsx
+++ therapy-app/src/app/_layout.tsx
@@ -1,5 +1,9 @@
+import { useEffect } from 'react';
 import { Stack } from 'expo-router';
+import { Platform } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as NavigationBar from 'expo-navigation-bar';
+import { StatusBar } from 'expo-status-bar';
 import 'react-native-url-polyfill/auto';
 // Ce polyfill ajoute certaines fonctions web (URL, atob, etc.)
 // qui ne sont pas toujours disponibles en React Native.
@@ -9,8 +13,26 @@ import 'react-native-url-polyfill/auto';
 // une seule fois au démarrage de l’application.
 
 export default function RootLayout() {
+  useEffect(() => {
+    const run = async () => {
+      if (Platform.OS !== 'android') return;
+
+      try {
+        await NavigationBar.setBackgroundColorAsync('#FFFFFF');
+        await NavigationBar.setButtonStyleAsync('dark');
+      } catch (e) {
+        console.log('NavigationBar error:', e);
+      }
+    };
+
+    run();
+  }, []);
+
   return (
     <SafeAreaProvider>
+      {/* Haut : icônes (wifi/batterie/heure) */}
+      <StatusBar style="dark" backgroundColor="#FFFFFF" />
+
       <Stack screenOptions={{ headerShown: false }} />
     </SafeAreaProvider>
   );
